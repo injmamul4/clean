67e1ce634db59396401dcb603288246599f024e3
injmamul4 <injmamul1365@gmail.com>
2025-03-10 11:11:03 +0530

    Add files via upload

---
+++ new task/parectice/1.js
@@ -0,0 +1,26 @@
+// for (let a = 10; a <= 20; a++) {
+//    if (a%2===0) {
+//     console.log(a)
+//    }
+// }
+
+// let i = 10
+// while (i <=20) {
+//    console.log(i);
+//    i+=2;
+// }
+
+// let q =10;
+// do {
+//    console.log(q);
+
+//    q += 2;
+
+// } while (q<=20);
+
+
+for (let a = 10; a <= 20; a++) {
+   if (a % 2 === 0) {
+      console.log(a)
+   }
+}
